99f619d55d3c9db6668d898f24fb49b9f9254308
Kayfush <kayfush@gmail.com>
2018-02-01 02:25:47 +0300

    Add files via upload
    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.owl-carousel').owlCarousel({
+	loop: true,
+	items: 1,
+	margin: 0,
+	nav: true,
+	dots: false,
+	navText: ['<img src="images/prev.png" alt="prev">', '<img src="images/next.png" alt="next">'],
+})
